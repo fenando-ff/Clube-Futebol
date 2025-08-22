7b25271efe553e8dad01876554dd368017ecf938
Fernando Freitas <fernandofreitassud2016@gmail.com>
2025-08-22 02:07:08 -0300

    Update deletar_categoria_produto.sql

--- db/scripts_db/procedures/deletar_categoria_produto.sql
+++ db/scripts_db/procedures/deletar_categoria_produto.sql
@@ -2,8 +2,8 @@ DELIMITER //
 CREATE PROCEDURE deletarCategoriaProduto(in p_idCategoria int)
 	BEGIN
 		DELETE FROM categoria_produtos
-        WHERE p_idCategoria;
+        WHERE id_CATEGORIA_PRODUTOS = p_idCategoria;
     END //
 DELIMITER ;
-
+drop procedure deletarCategoriaProduto;
 call deletarCategoriaProduto(6)
